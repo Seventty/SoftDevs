c17373aba7494bacb3b59a5b2fbe44c71d7e2465
seventty <elmastigeron2015@gmail.com>
2021-07-10 23:48:33 -0400

    new image added to background

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import './App.css';
 const useStyles = makeStyles((theme)=> ({
   root: {
     minHeight: '100vh',
-    backgroundImage:`url(${process.env.PUBLIC_URL + '/assets/Img1.jpeg'})`,
+    backgroundImage:`url(${process.env.PUBLIC_URL + '/assets/background.png'})`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     backgroundPosition: '5% 50%',
     [theme.breakpoints.down('md')]: {
-      backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg1md.jpg'})`,
+      backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/background.png'})`,
       backgroundRepeat: 'no-repeat',
       backgroundSize: 'cover',
       backgroundPosition: 'center center'
